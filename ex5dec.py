0fe57157600c68c9944e57ad56d74edd73d01dc5
milan9725 <parvadiyamilan9@gmail.com>
2024-01-08 17:54:38 +0530

    Program

---
+++ ex5dec.py
@@ -0,0 +1,13 @@
+l=[30,False,"Python",100,200,"Java",21,24,1,2,3,1.1,2.2,"Tops",True,10,20]
+
+print(l[3:13]) 
+print(l[:15])
+print(l[5:])
+print(l[1:15:4])
+print(l[::5])
+
+print(l[-15: -5])
+print(l[:-1])
+print(l[-14:])
+print(l[-16: -3 :3])
+print(l[::-3]) 
