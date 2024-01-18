4a4dfdb5537cbe25aec10ea4596a321fc4950656
milan9725 <parvadiyamilan9@gmail.com>
2024-01-18 18:34:26 +0530

    Program

---
+++ maxnumber.py
@@ -0,0 +1,13 @@
+a=int(input("enter a : "))
+b=int(input("enter b : "))
+c=int(input("enter c : "))
+
+if a>b:
+    if a>c:
+        printf("a is max number")
+    else:
+        printf("c is max number")
+elif b>c:
+    print("b is max number")
+else:
+    print("c is max number")
