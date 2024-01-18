f92cc9dd52fe632f274848ed0885726eb07832b4
milan9725 <parvadiyamilan9@gmail.com>
2024-01-18 18:33:08 +0530

    Program

---
+++ list.py
@@ -0,0 +1,14 @@
+l=[1,2,1.1,2.2,"tops","python",1,2,10,20,False]
+
+print(l)
+l.append(100)
+print(l)
+l.clear()
+print(1)
+ll=l.copy()
+print(11)
+ll.append(200)
+print(11)
+print(1)
+l2=l
+print(12)
